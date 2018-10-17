7cbfa4e7be9ef5d59522f19d47a8a0cc8b45059a
Günther Neumair <g.neumair@tuta.io>
2018-10-17 07:22:57 +0200

    Add python bindings to new lite version

--- include/nyumaya_lib.h
+++ include/nyumaya_lib.h
@@ -9,16 +9,40 @@ class AudioRecognition {
 	public:
 
 		AudioRecognition(const std::string& modelPath);
+
 		~AudioRecognition();
 
 		int RunDetection(int16_t*data,const int array_length);
-		void SetSensitivity(float sens);
-		void SetGain(float val);
 
+		//Set the detection sensitivity between 0-1
+		//0 low sensitivity
+		//1 high sensitivity
+		void SetSensitivity(float sens);
+
+		//Set the volume gain to be applied to the input signal
+		void SetGain(float val);
+		
 		size_t GetInputDataSize();
 		std::string GetVersionString();
 	private:
 		AudioRecognitionImpl* mImpl;
 };
+
+
+
+
+extern "C"
+{
+	AudioRecognitionImpl* create_audio_recognition(const char* modelPath);
+	int RunDetection(AudioRecognitionImpl*impl,int16_t*data,const int array_length); 
+	void SetSensitivity(AudioRecognitionImpl*impl,float sens);
+ 	void SetGain(AudioRecognitionImpl*impl,float val);
+	size_t GetInputDataSize(AudioRecognitionImpl*impl);
+	char* GetVersionString(AudioRecognitionImpl*impl);
+}
+
+
+
+
 
 #endif

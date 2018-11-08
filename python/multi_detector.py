52dea899cf993ad2f1e43da5e04a88e14e737f8c
Günther Neumair <g.neumair@tuta.io>
2018-11-08 22:58:52 +0100

    Rerun last 5 frames on next detector, reduce detector list creation

--- python/multi_detector.py
+++ python/multi_detector.py
@@ -9,12 +9,7 @@ from ringbuffer import RingBuffer
 
 #TODO: Optimize: When running multiple detectors feature extraction has to be done only once
 
-#TODO: Optimize: When changing detectors rerun the last 500ms in the new detector to better
-#                recorgnize commands without a pause
-
 #TODO: Optimize: More error handling
-
-#TODO: Optimize: Creating detector and word list can be reduced
 
 
 class MultiDetector():
@@ -29,14 +24,23 @@ class MultiDetector():
 		self.commands  = []
 		self.libpath = libpath
 		self.history=[]
+		self.last_frames = []
+		self.max_last_frames = 5
 		
 	def get_possible_words(self,history):
 		
 		words = []
 		for cmd in self.commands:
 			index = self.command_starts_with_history(cmd['command'],history)
+			
+			if(index >= len(cmd['command'])):
+				print ("Error index out of range:")
+				print ("Command: " + str(cmd))
+				print ("Index: " + str(index))
+				print ("History: " + str(history))
+				return []
+				
 			if(index >=0):
-	
 				cmd = cmd['command'][index]
 				if(not cmd in words):
 					words.append(cmd)
@@ -60,6 +64,10 @@ class MultiDetector():
 		for detector in self.detectors:
 			detector.RemoveDC(value)
 
+	def UpdateLastFrames(self,frame):
+		self.last_frames.append(frame)
+		if len(self.last_frames) > self.max_last_frames:
+			self.last_frames.pop(0)
 
 	def command_starts_with_history(self,cmd,history):
 		index = 0
@@ -78,6 +86,7 @@ class MultiDetector():
 			return 
 
 		self.commands.append({'command':command.split(","), 'function': callback_function })
+		self.update_word_and_detector()
 
 	def add_detector(self,graph,labels,sensitivity):
 		detector = AudioRecognition(self.libpath,graph,labels)
@@ -88,35 +97,55 @@ class MultiDetector():
 		return self.detectors[0].GetInputDataSize()
 
 	def maby_execute(self):
+		executed_cmd = False
 		for cmd in self.commands:
 			if(cmd['command'] == self.history):
 				cmd['function']()
 				self.history = []
 				self.countdown = 0
-				
+				self.last_frames = []
+				executed_cmd =  True
+			
+		return executed_cmd
+			
 	def check_timeout(self):
 
 		if(self.countdown > 0):
 			self.countdown -= 1
 			if(self.countdown == 0):
 				self.history = []
+				self.update_word_and_detector()
 				print("Reset History")
 
-	def run_frame(self,frame):
-		possible_words = self.get_possible_words(self.history)
-		current_detectors = self.get_detectors_for_words(possible_words)
-		
+	def update_word_and_detector(self):
+		self.possible_words = self.get_possible_words(self.history)
+		self.current_detectors = self.get_detectors_for_words(self.possible_words)
+
+	def run_frame(self,frame,update_frames=True):
+
+		if(update_frames):
+			self.UpdateLastFrames(frame)
+			
 		self.check_timeout()
 		
-		for detector in current_detectors:
+		for detector in self.current_detectors:
 			prediction = detector.RunDetection(frame)
 			if(prediction):
 				label = detector.GetPredictionLabel(prediction)
-				if(label in possible_words):
+				if(label in self.possible_words):
 					print("Got prediction: " + label)
 					self.countdown = self.timeout
 					self.history.append(label)
-					self.maby_execute()
+					result = self.maby_execute()
+					self.update_word_and_detector()
+					
+					#Command hasn't finished so run last frames in next detectors
+					if(not result):
+						self.run_last_frames()
+	
+	def run_last_frames(self):
+		for frame in self.last_frames:
+			self.run_frame(frame,update_frames=False)
 
 	def print_commands(self):
 		for cmd in self.commands:

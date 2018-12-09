566b8d95db6f73c16ac0991223e88df58a16edf9
Günther Neumair <g.neumair@tuta.io>
2018-12-09 16:08:38 +0100

    Prepare C code for version 0.3

--- include/nyumaya_lib.h
+++ include/nyumaya_lib.h
@@ -4,41 +4,45 @@
 #include <string>
 
 class AudioRecognitionImpl;
-class AudioRecognition {
-
-	public:
-
-		AudioRecognition(const std::string& modelPath);
-
-		~AudioRecognition();
-
-		int RunDetection(int16_t*data,const int array_length);
-
-		//Set the detection sensitivity between 0-1
-		//0 low sensitivity
-		//1 high sensitivity
-		void SetSensitivity(float sens);
-
-		//Set the volume gain to be applied to the input signal
-		void SetGain(float val);
-		
-		size_t GetInputDataSize();
-		std::string GetVersionString();
-	private:
-		AudioRecognitionImpl* mImpl;
-};
-
-
-
+class SpeakerVerificationImpl;
+class FeatureExtractor;
 
 extern "C"
 {
+
+
+	const char* GetVersionString(){return "0.0.3";}
+
+	//Audio Recognition
 	AudioRecognitionImpl* create_audio_recognition(const char* modelPath);
-	int RunDetection(AudioRecognitionImpl*impl,int16_t*data,const int array_length); 
+	
+	//@param array_length    Number of int16_t samples
+	//@param data            Signed int16 pcm data
+	int RunDetection(AudioRecognitionImpl*impl,const uint8_t* const data,const int mel_length); 
+	
 	void SetSensitivity(AudioRecognitionImpl*impl,float sens);
- 	void SetGain(AudioRecognitionImpl*impl,float val);
+ 	
 	size_t GetInputDataSize(AudioRecognitionImpl*impl);
-	char* GetVersionString(AudioRecognitionImpl*impl);
+	
+
+	//Speaker Verification
+	SpeakerVerificationImpl* create_speaker_verification(const char*modelPath);
+
+	float* VerifySpeaker(SpeakerVerificationImpl*impl,const int16_t* const data,const int array_length);
+	
+	
+	
+	//Feature Extractor
+	FeatureExtractor* create_feature_extractor(size_t nfft=512,size_t melcount = 40,size_t sample_rate=16000,
+	    size_t lowerf=20, size_t upperf=8000,float window_len=0.03,float shift=0.01);
+
+	int signal_to_mel(FeatureExtractor*impl,const int16_t * const pcm, size_t len,uint8_t*result,float gain);
+	
+	size_t get_melcount(FeatureExtractor*impl);
+		
+	void remove_dc_offset(FeatureExtractor*impl,bool value);
+	
+
 }
 
 

faf4b9bc45e06a75b80f4b2eaa2f2aa379275295
Günther Neumair <g.neumair@tuta.io>
2018-12-28 00:06:27 +0100

    Fix dummy sample

--- cpp/dummy_sample.cpp
+++ cpp/dummy_sample.cpp
@@ -7,9 +7,13 @@
 
 int main (int argc, char *argv[])
 {
+	AudioRecognitionImpl *audio_recognizer = create_audio_recognition(argv[1]);
+	SetSensitivity(audio_recognizer,0.5);
+	int buffer_size = GetInputDataSize(audio_recognizer);
+	
+	FeatureExtractor* feature_extractor = create_feature_extractor();
 
-	AudioRecognition *ar = new AudioRecognition(argv[1]);
-	//ar->ProfileRun();
+	char *buffer = (char*) malloc(buffer_size);
 
 	clock_t begin = clock();
 
@@ -20,8 +24,14 @@ int main (int argc, char *argv[])
 		{
 			data_test[i] = (std::rand() - 16384)/1000.0 ;
 		}
-		ar->RunDetection(data_test,3200);
-		std::cout << "OK: " << i << std::endl;
+		
+		
+		uint8_t melfeatures[800];
+		float gain = 1.0;
+		int mel_len = signal_to_mel(feature_extractor,(int16_t*)buffer,buffer_size/2,melfeatures,gain);
+		
+		int res = RunDetection(audio_recognizer,melfeatures,mel_len);
+		std::cout << "OK: " << res << std::endl;
 	}
 
 

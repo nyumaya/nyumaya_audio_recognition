ae597f11225d303b963082a5e617b0983344f34f
Günther Neumair <g.neumair@tuta.io>
2019-01-01 23:19:26 +0100

    Add simple test script

---
+++ python/test_mini.sh
@@ -0,0 +1,23 @@
+
+if [ "$(uname -m | grep armv6l -c)" -eq 1 ]; then
+    echo "Check for PiZero"
+    LIBPATH="../lib/rpi/armv6/libnyumaya.so"
+fi
+
+if [ "$(uname -m | grep armv7l -c)" -eq 1 ]; then
+    echo "Check for Pi3"
+    LIBPATH="../lib/rpi/armv7/libnyumaya.so"
+fi
+
+if [ "$(uname -m | grep x86_64 -c)" -eq 1 ]; then
+    echo "Check for X86_64"
+    LIBPATH="../lib/linux/libnyumaya.so"
+fi
+
+
+
+
+python test_accuracy.py --graph ../models/Hotword/marvin_small_0.3.tflite \
+                        --labels ../models/Hotword/marvin_labels.txt \
+                        --good_folder ../../nyumaya_audio_testdata/nyumaya_marvin_test_0_1/nyumaya_marvin_test_0.1/nyumaya_marvin_ff_test/marvin \
+                        --libpath $LIBPATH

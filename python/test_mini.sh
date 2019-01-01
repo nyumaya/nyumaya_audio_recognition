f50bcab931cad116b9d0e50e3f8a31bc9a117204
Günther Neumair <g.neumair@tuta.io>
2019-01-02 00:49:10 +0100

    Travis test fixes

--- python/test_mini.sh
+++ python/test_mini.sh
@@ -1,19 +1,28 @@
+if [ "$OSTYPE" = "linux-gnu" ]; then
 
-if [ "$(uname -m | grep armv6l -c)" -eq 1 ]; then
-    echo "Check for PiZero"
-    LIBPATH="../lib/rpi/armv6/libnyumaya.so"
+    if [ "$(uname -m | grep armv6l -c)" -eq 1 ]; then
+        echo "Check for PiZero"
+        LIBPATH="../lib/rpi/armv6/libnyumaya.so"
+    fi
+
+    if [ "$(uname -m | grep armv7l -c)" -eq 1 ]; then
+        echo "Check for Pi3"
+        LIBPATH="../lib/rpi/armv7/libnyumaya.so"
+    fi
+
+    if [ "$(uname -m | grep x86_64 -c)" -eq 1 ]; then
+        echo "Check for X86_64"
+        LIBPATH="../lib/linux/libnyumaya.so"
+    fi
+    
+elif [ "$OSTYPE" = "darwin"* ]; then
+     echo "Check for OSX"
+     LIBPATH="/usr/local/lib/libnyumaya.so"
+
+else
+    echo "Unknown OS: $OSTYPE"
+    exit
 fi
-
-if [ "$(uname -m | grep armv7l -c)" -eq 1 ]; then
-    echo "Check for Pi3"
-    LIBPATH="../lib/rpi/armv7/libnyumaya.so"
-fi
-
-if [ "$(uname -m | grep x86_64 -c)" -eq 1 ]; then
-    echo "Check for X86_64"
-    LIBPATH="../lib/linux/libnyumaya.so"
-fi
-
 
 
 

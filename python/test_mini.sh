1a0f21893d0dc422ef6f22c0f965374ed1fd4374
Günther Neumair <g.neumair@tuta.io>
2019-01-02 04:07:47 +0100

    Test both python and python3

--- python/test_mini.sh
+++ python/test_mini.sh
@@ -34,3 +34,10 @@ python test_accuracy.py --graph ../models/Hotword/marvin_small_0.3.tflite \
                         --noise_folders ../../nyumaya_audio_testdata/nyumaya_mic_noise \
                         --bad_folders "../../nyumaya_audio_testdata/cv_mini,../../nyumaya_audio_testdata/nyumaya_mic_noise" \
                         --libpath $LIBPATH
+                        
+python3 test_accuracy.py --graph ../models/Hotword/marvin_small_0.3.tflite \
+                        --labels ../models/Hotword/marvin_labels.txt \
+                        --good_folder ../../nyumaya_audio_testdata/nyumaya_marvin_test_0.1/nyumaya_marvin_ff_test/marvin \
+                        --noise_folders ../../nyumaya_audio_testdata/nyumaya_mic_noise \
+                        --bad_folders "../../nyumaya_audio_testdata/cv_mini,../../nyumaya_audio_testdata/nyumaya_mic_noise" \
+                        --libpath $LIBPATH

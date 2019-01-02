546ac5b271779ac3e63e64ba9ac1a8c72fdd9e23
Günther Neumair <g.neumair@tuta.io>
2019-01-02 01:15:46 +0100

    Improved OS check

--- python/test_mini.sh
+++ python/test_mini.sh
@@ -1,5 +1,16 @@
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
 
+    if [ "$(uname -m | grep x86_64 -c)" -eq 1 ]; then
+        echo "Check for X86_64"
+        LIBPATH="../lib/linux/libnyumaya.so"
+    fi
+    
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+     echo "Check for OSX"
+     LIBPATH="/usr/local/lib/libnyumaya.dylib"
+
+elif [[ "$OSTYPE" == "linux-gnueabihf" ]]; then
+
     if [ "$(uname -m | grep armv6l -c)" -eq 1 ]; then
         echo "Check for PiZero"
         LIBPATH="../lib/rpi/armv6/libnyumaya.so"
@@ -9,15 +20,6 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
         echo "Check for Pi3"
         LIBPATH="../lib/rpi/armv7/libnyumaya.so"
     fi
-
-    if [ "$(uname -m | grep x86_64 -c)" -eq 1 ]; then
-        echo "Check for X86_64"
-        LIBPATH="../lib/linux/libnyumaya.so"
-    fi
-    
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-     echo "Check for OSX"
-     LIBPATH="/usr/local/lib/libnyumaya.dylib"
 
 else
     echo "Unknown OS: $OSTYPE"

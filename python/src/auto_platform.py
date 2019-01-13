0268c893c89f56822b44fbff7701a1921ac65bf3
Günther Neumair <g.neumair@tuta.io>
2019-01-13 22:11:39 +0100

    Code to detect plaform

---
+++ python/src/auto_platform.py
@@ -0,0 +1,40 @@
+import time
+import os
+import argparse
+import sys
+import datetime
+import platform
+
+play_command = None
+default_libpath = None
+
+system = platform.system()
+release = platform.release()
+machine = platform.machine()
+uname = platform.uname()
+
+print("System:" + system)
+print("Release:" + release)
+print("Machine:" + machine)
+print("Uname:" + str(uname))
+
+
+if system == "Darwin":
+	play_command ="play -q"
+	
+	from cross_record import AudiostreamSource
+	default_libpath = '../lib/mac/libnyumaya.dylib'
+	
+elif system == "Linux":
+	from record import AudiostreamSource
+	play_command = "aplay"
+	if(machine == "x86_64"):
+		default_libpath = '../lib/linux/libnyumaya.so'
+	else:
+		print("Machine not supported")
+		
+elif system == "Windows":
+	print("Windows is currently not supported")
+
+
+

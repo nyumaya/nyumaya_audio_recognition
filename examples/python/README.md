a9304a2d8095980070f3ba68e72879c8d9c603fb
Günther Neumair <g.neumair@tuta.io>
2022-05-08 21:57:06 +0200

    Add vosk example

---
+++ examples/python/README.md
@@ -0,0 +1,20 @@
+# simple_hotword.py
+Basic demo of detecting hotwords without doing any special actions
+
+
+# record_activations.py
+Useful application for recording activations into wav files
+
+
+# vosk_with_keyword.py
+Demo of application running the nyumaya keyword and passing the
+control to vosk for further actions (in this example recognizing
+numbers one to ten)
+
+```
+pip3 install vosk
+wget https://alphacephei.com/kaldi/models/vosk-model-small-en-us-0.15.zip
+unzip vosk-model-small-en-us-0.15.zip
+mv vosk-model-small-en-us-0.15 model
+```
+
---
+++ examples/python/vosk_with_keyword.py
@@ -0,0 +1,83 @@
+import os
+import argparse
+import sys
+import datetime
+import time
+from vosk import Model, KaldiRecognizer
+
+
+sys.path.append('../../python/src')
+
+from libnyumaya import AudioRecognition, FeatureExtractor
+from auto_platform import AudiostreamSource, play_command, default_libpath
+
+def detectKeywords(libpath):
+
+	audio_stream = AudiostreamSource()
+	extractor = FeatureExtractor(libpath)
+	detector = AudioRecognition(libpath)
+
+	framerate = 16000
+	model = Model("model")
+
+	#Let's define a custom dictionary
+	rec = KaldiRecognizer(model, framerate, '["oh one two three four five six seven eight nine zero", "[unk]"]')
+
+	extactor_gain = 1.0
+
+	#Add one or more keyword models
+	keywordIdAlexa = detector.addModel('../../models/Hotword/alexa_v3.0.35.premium',0.85)
+
+	bufsize = detector.getInputDataSize()
+
+	print("Audio Recognition Version: " + detector.getVersionString())
+
+	command_started = False
+
+	audio_stream.start()
+	try:
+		while(True):
+			# Wakeword loop
+			if(not command_started):
+				frame = audio_stream.read(bufsize*2,bufsize*2)
+				if(not frame):
+					time.sleep(0.01)
+					continue
+
+				features = extractor.signalToMel(frame,extactor_gain)
+				prediction = detector.runDetection(features)
+				if(prediction != 0):
+					now = datetime.datetime.now().strftime("%d.%b %Y %H:%M:%S")
+					if(prediction == keywordIdAlexa):
+						print("Alexa detected:" + now)
+
+					os.system(play_command + " ../resources/ding.wav")
+					command_started = True
+			# vosk loop
+			else:
+				frame = audio_stream.read(4000,4000)
+				if(not frame):
+					time.sleep(0.01)
+					continue
+
+				if rec.AcceptWaveform(bytes(frame)):
+					print(rec.Result())
+					command_started = False
+					print(rec.FinalResult())
+
+	except KeyboardInterrupt:
+		print("Terminating")
+		audio_stream.stop()
+		sys.exit(0)
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+
+	parser.add_argument(
+		'--libpath', type=str,
+		default=default_libpath,
+		help='Path to Platform specific nyumaya_lib.')
+
+	FLAGS, unparsed = parser.parse_known_args()
+
+	detectKeywords(FLAGS.libpath)
